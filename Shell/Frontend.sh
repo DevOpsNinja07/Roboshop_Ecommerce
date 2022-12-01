a3110a5812799d8ddbc748e20d4c8a78a7ceb780
DevOpsNinja07 <devopsninja07@gmail.com>
2022-12-01 14:23:23 +0530

    Shellcommit#2

--- Shell/Frontend.sh
+++ Shell/Frontend.sh
@@ -2,13 +2,13 @@
 yum install ngnix -y
 systemctl enable nginx
 systemctl start nginx
-##download the HTDOCS content and deploy under the Nginx path
+#download the HTDOCS content and deploy under the Nginx path
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-##Deploy the downloaded content in Nginx Default Location
+#Deploy the downloaded content in Nginx Default Location
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-##Restart service.
+#Restart service.
 systemctl restart nginx
