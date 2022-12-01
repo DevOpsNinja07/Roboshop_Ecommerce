3eee3a8e17d06ae1c0ce7601cd367a885b21835f
DevOpsNinja07 <devopsninja07@gmail.com>
2022-12-01 15:30:40 +0530

    Mongo script#automated

---
+++ Shell/catalogue.sh
@@ -0,0 +1,16 @@
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+rm -rf catalogue
+unzip -o  /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
+
+sed -i -e 's/MONGO_DNSNAME/mymongodns.dns/' systemd.service
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
