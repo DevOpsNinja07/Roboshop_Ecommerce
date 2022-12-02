fa9edc55c0735032ee7d8ebb20dfe6025d1aae58
DevOpsNinja07 <devopsninja07@gmail.com>
2022-12-02 11:28:12 +0530

    mysql condition status check #modified

--- Shell/mysql.sh
+++ Shell/mysql.sh
@@ -1,46 +1,44 @@
+echo Downloading Repo
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? -eq 0]; then
+  echo SUCESS
+else
+  echo FAILURE
+fi
 
-if [$? -eq 0];
-then
-  echo SUCESS
-else
-  echo FAILURE
-fi
+echo Disabling mysql v8
 dnf module disable mysql -y
-if [$? -eq 0];
-then
+if [ $? -eq 0]; then
   echo SUCESS
 else
   echo FAILURE
 fi
+echo installing mysql
 yum install mysql-community-server -y
 
-if [$? -eq 0];
-then
+if [ $? -eq 0]; then
   echo SUCESS
 else
   echo FAILURE
 fi
+echo starting services
 systemctl enable mysqld
 systemctl start mysqld
-if [$? -eq 0];
-then
+if [ $? -eq 0]; then
   echo SUCESS
 else
   echo FAILURE
 fi
-echo show databases | mysql -uroot -pRoboshop@1
-if [ $? -ne 0];
-then
 
+echo show databases | mysql -uroot -pRoboshop@1
+if [ $? -ne 0]; then
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1' ; " > /tmp/root-pass-sql
 DEFAULTPASS=$(grep 'A temporary password ' /var/log/mysqld.log | awk '{print $NF}')
 cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULTPASS}"
 
 fi
-
-if [$? -eq 0]
-then
+echo password change
+if [ $? -eq 0]; then
   echo SUCESS
 else
   echo FAILURE
