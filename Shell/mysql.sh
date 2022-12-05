8ed0e296d55a3e81ac43dfb8be6c3c00494625b3
DevOpsNinja07 <devopsninja07@gmail.com>
2022-12-05 15:26:30 +0530

    Added functionality WET CODe

--- Shell/mysql.sh
+++ Shell/mysql.sh
@@ -7,35 +7,20 @@ RoboShopMYSQLPASS=$1
 
 PRINT Downloading Repo
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
-  echo SUCESS
-else
-  echo FAILURE
-fi
+STATISTICS $?
 
-echo Disabling mysql v8
+PRINT Disabling mysql v8
 dnf module disable mysql -y
-if [ $? -eq 0 ]; then
-  echo SUCESS
-else
-  echo FAILURE
-fi
-echo installing mysql
-yum install mysql-community-server -y
+STATISTICS $?
 
-if [ $? -eq 0 ]; then
-  echo SUCESS
-else
-  echo FAILURE
-fi
-echo starting services
+PRINT installing mysql
+yum install mysql-community-server -y
+STATISTICS $?
+
+PRINT  starting and enabling mysql services
 systemctl enable mysqld
 systemctl start mysqld
-if [ $? -eq 0 ]; then
-  echo SUCESS
-else
-  echo FAILURE
-fi
+STATISTICS $?
 
 echo show databases | mysql -uroot -p${RoboShopMYSQLPASS}
 if [ $? -ne 0 ]; then
@@ -44,9 +29,5 @@ DEFAULTPASS=$(grep 'A temporary password ' /var/log/mysqld.log | awk '{print $NF
 cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULTPASS}"
 
 fi
-echo password change
-if [ $? -eq 0 ]; then
-  echo SUCESS
-else
-  echo FAILURE
-fi
+PRINT password change
+STATISTICS $?
