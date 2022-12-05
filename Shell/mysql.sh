3f8bd2a87949bc94dde6ea1c7ff8609344079f2d
DevOpsNinja07 <devopsninja07@gmail.com>
2022-12-05 15:16:12 +0530

    Added input argument

--- Shell/mysql.sh
+++ Shell/mysql.sh
@@ -1,3 +1,9 @@
+if [ -z "$1" ]; then
+  echo "INPUT PASSWORD ARGUMENT!!"
+  exit
+fi
+RoboShopMYSQLPASS=$1
+
 echo Downloading Repo
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 if [ $? -eq 0 ]; then
@@ -30,9 +36,9 @@ else
   echo FAILURE
 fi
 
-echo show databases | mysql -uroot -pRoboshop@1
+echo show databases | mysql -uroot -p${RoboShopMYSQLPASS}
 if [ $? -ne 0 ]; then
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1' ; " > /tmp/root-pass-sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${RoboShopMYSQLPASS}' ; " > /tmp/root-pass-sql
 DEFAULTPASS=$(grep 'A temporary password ' /var/log/mysqld.log | awk '{print $NF}')
 cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULTPASS}"
 
